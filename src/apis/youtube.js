82aeacae2f0168d446561c0cbbaf812fe1b53788
jackerekson <erekson.jack@gmail.com>
2021-10-03 00:10:49 -0500

    first commit

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyA4ZuDwBtoYtyF9RCBrzg8mpRyIrlyTX4o'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: KEY
+    }
+})
